ce20909e2c581b020ab8ee5556547b31956fdd63
Iqbal <89783237+IqbalManazilYuni@users.noreply.github.com>
2024-04-17 21:18:04 +0700

    commit

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,11 @@
 import { Sequelize } from "sequelize";
+import dotenv from 'dotenv'; // Impor dotenv untuk memuat variabel lingkungan
 
-const db = new Sequelize('recruitment', 'root','',{
-    host: 'localhost',
-    dialect: 'mysql',
+dotenv.config(); // Memuat variabel lingkungan dari file .env
 
-})
+const db = new Sequelize(process.env.DB_DBNAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql'
+});
 
 export default db;
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import cors from "cors";
 import UserRoutes from './routes/UserRoutes.js';
+import dotenv from 'dotenv';
 // import db from './config/db.config.js';
-
+dotenv.config(); 
 const app = express();
 
 // (async () => {
@@ -13,6 +14,6 @@ app.use(cors());
 app.use(express.json());
 app.use(UserRoutes);
 
-app.listen(3001, () => {
-  console.log("listening on ")
+app.listen(process.env.PORT, () => {
+  console.log("listening on", process.env.PORT);
 })
