8e553a90debedd062ed76042fbcce4d406887ace
Iqbal <89783237+IqbalManazilYuni@users.noreply.github.com>
2024-04-17 21:28:58 +0700

    update

--- index.js
+++ index.js
@@ -9,11 +9,32 @@ const app = express();
 // (async () => {
 //   await db.sync();
 // })();
+// Menggunakan dotenv untuk konfigurasi
+const sequelize = new Sequelize({
+  dialect: 'mysql',
+  host: 'bfrl0tflajs99qxjy82q-mysql.services.clever-cloud.com',
+  port: 3306,
+  username: 'umjokrsq2nnxdyke',
+  password: 'Fs1a3Cp6uZwinKEGLCLT', // Ganti dengan password MySQL Anda
+  database: 'bfrl0tflajs99qxjy82q'
+});
+
+// Cek koneksi ke database
+try {
+  await sequelize.authenticate();
+  console.log('Connection to the database has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
+// Definisikan model-model Sequelize di sini (jika diperlukan)
+// Contoh:
+// const User = sequelize.define('User', { /* definisi model */ });
 
 app.use(cors());
 app.use(express.json());
 app.use(UserRoutes);
 
 app.listen(process.env.PORT, () => {
-  console.log("listening on", process.env.PORT);
-})
+  console.log("Listening on port", process.env.PORT);
+});
