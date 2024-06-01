a0128ae461fdb7610ba785530e120205239090b8
Iqbal <89783237+IqbalManazilYuni@users.noreply.github.com>
2024-06-01 14:58:14 +0700

    push

--- config/db.config.js
+++ config/db.config.js
@@ -1,16 +1,16 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv'; // Impor dotenv untuk memuat variabel lingkungan
 
 dotenv.config(); // Memuat variabel lingkungan dari file .env
 
 const db = new Sequelize({
   dialect: 'mysql',
+  dialectModule: require('mysql2'), // Menambahkan ini untuk menggunakan mysql2
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DBNAME
 });
-
 
 export default db;
